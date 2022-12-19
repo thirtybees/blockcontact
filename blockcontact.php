af340437976775753380f497b16e2d85de9d59b1
Petr Hučík <petr@getdatakick.com>
2022-12-19 11:00:34 +0100

    Code style
    - added braces
    - used short array syntax

--- blockcontact.php
+++ blockcontact.php
@@ -23,8 +23,9 @@
  * PrestaShop is an internationally registered trademark of PrestaShop SA.
  */
 
-if (!defined('_CAN_LOAD_FILES_'))
+if (!defined('_CAN_LOAD_FILES_')) {
     exit;
+}
 
 class Blockcontact extends Module
 {
@@ -47,7 +48,7 @@ class Blockcontact extends Module
         $this->description = $this->l('Allows you to add additional information about your store\'s customer service.');
         $this->tb_versions_compliancy = '> 1.0.0';
         $this->tb_min_version = '1.0.0';
-        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => '1.6.99.99');
+        $this->ps_versions_compliancy = ['min' => '1.6', 'max' => '1.6.99.99'];
     }
 
     /**
@@ -123,13 +124,15 @@ class Blockcontact extends Module
     public function hookDisplayRightColumn($params)
     {
         $tpl = 'blockcontact';
-        if (isset($params['blockcontact_tpl']) && $params['blockcontact_tpl'])
+        if (isset($params['blockcontact_tpl']) && $params['blockcontact_tpl']) {
             $tpl = $params['blockcontact_tpl'];
-        if (!$this->isCached($tpl . '.tpl', $this->getCacheId()))
-            $this->context->smarty->assign(array(
+        }
+        if (!$this->isCached($tpl . '.tpl', $this->getCacheId())) {
+            $this->context->smarty->assign([
                 'telnumber' => Configuration::get('BLOCKCONTACT_TELNUMBER'),
                 'email' => Configuration::get('BLOCKCONTACT_EMAIL')
-            ));
+            ]);
+        }
         return $this->display(__FILE__, $tpl . '.tpl', $this->getCacheId());
     }
 
@@ -170,33 +173,33 @@ class Blockcontact extends Module
      */
     public function renderForm()
     {
-        $fields_form = array(
-            'form' => array(
-                'legend' => array(
+        $fields_form = [
+            'form' => [
+                'legend' => [
                     'title' => $this->l('Settings'),
                     'icon' => 'icon-cogs'
-                ),
+                ],
                 'description' => $this->l('This block displays in the header your phone number (‘Call us now’), and a link to the ‘Contact us’ page.') . '<br/><br/>' .
                     $this->l('To edit the email addresses for the ‘Contact us’ page: you should go to the ‘Contacts’ page under the ‘Customer’ menu.') . '<br/>' .
                     $this->l('To edit the contact details in the footer: you should go to the ‘Contact Information Block’ module.'),
-                'input' => array(
-                    array(
+                'input' => [
+                    [
                         'type' => 'text',
                         'label' => $this->l('Telephone number'),
                         'name' => 'blockcontact_telnumber',
-                    ),
-                    array(
+                    ],
+                    [
                         'type' => 'text',
                         'label' => $this->l('Email'),
                         'name' => 'blockcontact_email',
                         'desc' => $this->l('Enter here your customer service contact details.'),
-                    ),
-                ),
-                'submit' => array(
+                    ],
+                ],
+                'submit' => [
                     'title' => $this->l('Save'),
-                )
-            ),
-        );
+                ]
+            ],
+        ];
 
         $helper = new HelperForm();
         $helper->show_toolbar = false;
@@ -204,19 +207,19 @@ class Blockcontact extends Module
         $lang = new Language((int)Configuration::get('PS_LANG_DEFAULT'));
         $helper->default_form_language = $lang->id;
         $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
-        $this->fields_form = array();
+        $this->fields_form = [];
 
         $helper->identifier = $this->identifier;
         $helper->submit_action = 'submitModule';
         $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false) . '&configure=' . $this->name . '&tab_module=' . $this->tab . '&module_name=' . $this->name;
         $helper->token = Tools::getAdminTokenLite('AdminModules');
-        $helper->tpl_vars = array(
+        $helper->tpl_vars = [
             'fields_value' => $this->getConfigFieldsValues(),
             'languages' => $this->context->controller->getLanguages(),
             'id_language' => $this->context->language->id
-        );
+        ];
 
-        return $helper->generateForm(array($fields_form));
+        return $helper->generateForm([$fields_form]);
     }
 
     /**
@@ -226,9 +229,9 @@ class Blockcontact extends Module
      */
     public function getConfigFieldsValues()
     {
-        return array(
+        return [
             'blockcontact_telnumber' => Tools::getValue('blockcontact_telnumber', Configuration::get('BLOCKCONTACT_TELNUMBER')),
             'blockcontact_email' => Tools::getValue('blockcontact_email', Configuration::get('BLOCKCONTACT_EMAIL')),
-        );
+        ];
     }
 }
